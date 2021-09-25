28fb4c9b6830af35d5a88c9c952a9d97d185716f
Stephan Schmidt <stephans@bu.edu>
2021-09-25 16:33:26 -0400

    Added variable height to display function

--- 01-python/leisure/restaurant.py
+++ 01-python/leisure/restaurant.py
@@ -1,14 +1,34 @@
-def draw_restaurant():
-    print(
-    '''
-    ===============================
-    |                             |
-    |           Chipotle          |
-    |                             |
-    |             ====            |
-    |             |  |            |
-    |             |  |            |
-    ===============================
-    '''
-    )
+def draw_restaurant(height):
+    minimumHeight = 4
+    if (height <= 4):
+        print(
+'''
+===============================
+|           Chipotle          |
+|             ====            |
+|             |  |            |
+|             |  |            |
+===============================
+'''
+        )
+    else:
+        #print roof
+        print(
+'''
+===============================
+|           Chipotle          |'''
+        )
+        #print variable wall height
+        for x in range(height-minimumHeight):
+            print(
+'''|                             |'''
+            )
+        #print floor + entrance
+        print(
+'''|             ====            |
+|             |  |            |
+|             |  |            |
+===============================
+'''
+        )
     return
